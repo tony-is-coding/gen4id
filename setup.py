00378cbc5698eb8be93184323734a0a801332515
tony <newbiwtan@163.com>
2020-04-15 17:31:44 +0800

    delete describe.txt

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ try:
 except IOError:
     README = CHANGES = ''
 
+docs_extras = [
+    'Sphinx >= 1.3.1',
+]
+
 setup(
     name='gen4id',  # 包名字
-    version='1.0.2',  # 包版本
+    version='1.0.1',  # 包版本
     description='gen4id a simple but maybe useful package for auto-increment ids ',  # 简单描述
+    url='https://github.com/tony-is-coding/gen4id',
     author='tony-is-coding',  # 作者
     author_email='newbiwtan@163.com',  # 作者邮箱
     maintainer="tony-is-coing",
@@ -29,4 +34,7 @@ setup(
         "License :: OSI Approved :: MIT License",  # 根据MIT许可证开源
         "Operating System :: OS Independent",  # 与操作系统无关
     ],
+    extras_require={
+            'docs': docs_extras,
+        },
 )
